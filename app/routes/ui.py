7db34a4dbea4f128ff3260f58cf00613312c60bf
Ryan Bostian <rabos5@Mac.lan>
2025-11-06 18:00:40 -0500

    updating with altest

--- app/routes/ui.py
+++ app/routes/ui.py
@@ -14,9 +14,9 @@ logger = get_logger(FASTAPI_LOGGER_NAME, FASTAPI_LOG_LEVEL)
 api_router = APIRouter()
 
 
-@api_router.get("/sample_page_tailwind_css")
-async def get_sample_page_tailwind_css(request: Request) -> HTMLResponse:
-    with open(os.path.join(get_base_dir(), "static", "sample_page_tailwind_css.html"), "r") as f:
+@api_router.get("/sandbox")
+async def get_sandbox(request: Request) -> HTMLResponse:
+    with open(os.path.join(get_base_dir(), "static", "sandbox.html"), "r") as f:
         html_template = f.read()
         template = Template(html_template)
         rendered_html = template.render()
@@ -27,22 +27,9 @@ async def get_sample_page_tailwind_css(request: Request) -> HTMLResponse:
     )
 
 
-@api_router.get("/ai_experimentation_framework")
-async def get_ai_experimentation_framework(request: Request) -> HTMLResponse:
-    with open(os.path.join(get_base_dir(), "static", "ai_experimentation_framework.html"), "r") as f:
-        html_template = f.read()
-        template = Template(html_template)
-        rendered_html = template.render()
-
-    return HTMLResponse(
-        status_code=200,
-        content=rendered_html
-    )
-
-
-@api_router.get("/ai_single_chat")
-async def get_ai_single_chat(request: Request) -> HTMLResponse:
-    with open(os.path.join(get_base_dir(), "static", "ai_single_chat.html"), "r") as f:
+@api_router.get("/chat")
+async def get_chat(request: Request) -> HTMLResponse:
+    with open(os.path.join(get_base_dir(), "static", "chat.html"), "r") as f:
         html_template = f.read()
         template = Template(html_template)
         rendered_html = template.render()
